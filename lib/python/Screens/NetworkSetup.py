90cb200ba2f72b26e511d516c28e03cdc0609c15
Igornord <IgorkaG@gmail.com>
2015-03-13 14:01:41 +0300

    Revert "Move Wake On LAN config to NetworkSetup"

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -457,8 +457,6 @@ class AdapterSetup(Screen, ConfigListScreen, HelpableScreen):
 				self.list.append(self.gatewayEntry)
 				if self.hasGatewayConfigEntry.value:
 					self.list.append(getConfigListEntry(_('Gateway'), self.gatewayConfigEntry))
-			if SystemInfo["WakeOnLAN"] and self.iface == 'eth0':
-				self.list.append(getConfigListEntry(_('Enable Wake On LAN'), config.usage.wakeOnLAN))
 
 			self.extended = None
 			self.configStrings = None
