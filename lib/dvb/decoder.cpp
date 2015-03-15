1365330b43ce7f0bbe8fb0f8cd3aec0b1d8544af
Igornord <IgorkaG@gmail.com>
2015-03-15 13:09:58 +0300

    Звук для Pip (Thank's Taapat)

--- lib/dvb/decoder.cpp
+++ lib/dvb/decoder.cpp
@@ -850,7 +850,7 @@ int eTSMPEGDecoder::setState()
 	}
 	if (m_changed & changeAudio)
 	{
-		if ((m_apid >= 0) && (m_apid < 0x1FFF) && !noaudio)
+		if ((m_apid >= 0) && (m_apid < 0x1FFF) && !noaudio && (m_decoder == 0))
 		{
 			m_audio = new eDVBAudio(m_demux, m_decoder);
 			if (m_audio->startPid(m_apid, m_atype))
