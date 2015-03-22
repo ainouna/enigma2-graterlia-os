ed779f288cc1dac4af7cd396e24fb64a648bb600
Igornord <IgorkaG@gmail.com>
2015-03-22 11:48:37 +0300

    epgcache.cpp: Remove the public lies from eventData.

--- lib/dvb/epgcache.cpp
+++ lib/dvb/epgcache.cpp
@@ -41,17 +41,17 @@ struct DescriptorPair
 
 typedef std::map<__u32, DescriptorPair> DescriptorMap;
 
-class eventData
+struct eventData
 {
-	friend class eEPGCache;
-private:
 	__u8* EITdata;
 	__u8 ByteSize;
 	__u8 type;
 	static DescriptorMap descriptors;
 	static __u8 data[];
-	static int CacheSize;
+	static unsigned int CacheSize;
 	static bool isCacheCorrupt;
+	eventData(const eit_event_struct* e = NULL, int size = 0, int type = 0, int tsidonid = 0);
+	~eventData();
 	static void load(FILE *);
 	static void save(FILE *);
 	static void cacheCorrupt(const char* context);
@@ -60,9 +60,6 @@ private:
 	{
 		return get();
 	}
-public:
-	eventData(const eit_event_struct* e = NULL, int size = 0, int type = 0, int tsidonid = 0);
-	~eventData();
 	int getEventID()
 	{
 		return (EITdata[0] << 8) | EITdata[1];
@@ -78,7 +75,7 @@ public:
 };
 
 
-int eventData::CacheSize=0;
+unsigned int eventData::CacheSize = 0;
 bool eventData::isCacheCorrupt = 0;
 DescriptorMap eventData::descriptors;
 __u8 eventData::data[2 * 4096 + 12];
@@ -266,7 +263,7 @@ eventData::eventData(const eit_event_struct* e, int size, int type, int tsidonid
 	ASSERT(pdescr <= &descr[65]);
 	ByteSize = 10+((pdescr-descr)*4);
 	EITdata = new __u8[ByteSize];
-	CacheSize+=ByteSize;
+	CacheSize += ByteSize;
 	memcpy(EITdata, (__u8*) e, 10);
 	memcpy(EITdata+10, descr, ByteSize-10);
 }
@@ -277,6 +274,8 @@ const eit_event_struct* eventData::get() const
 	int tmp = ByteSize - 10;
 	memcpy(data, EITdata, 10);
 	unsigned int descriptors_length = 0;
+	/* Alignment error: EITdata + 10 is NOT on a 32-bit boundary, this
+	 * will cause a CPU exception on ARM for example */
 #ifndef __sh__
 	__u32 *p = (__u32*)(EITdata + 10);
 #else
@@ -319,10 +318,10 @@ eventData::~eventData()
 	{
 		CacheSize -= ByteSize;
 #ifndef __sh__
-		__u32 *d = (__u32*)(EITdata+10);
+		__u32 *d = (__u32*)(EITdata+10); /* Alignment error! */
 #else
 // Dagobert: fix not aligned access
-		__u8 *d = (__u8*)(EITdata+10);
+		__u8 *d = (__u8*)(EITdata+10); /* Alignment error! */
 #endif
 		ByteSize -= 10;
 		while (ByteSize > 3)
@@ -1333,7 +1332,7 @@ void eEPGCache::load()
 					fread( &len, sizeof(__u8), 1, f);
 					eventData *event = new eventData(0, len, type);
 					event->EITdata = new __u8[len];
-					eventData::CacheSize+=len;
+					eventData::CacheSize += len;
 					fread( event->EITdata, len, 1, f);
 					evMap[ event->getEventID() ]=event;
 					tmMap[ event->getStartTime() ]=event;
@@ -1440,7 +1439,7 @@ void eEPGCache::save()
 	tmp*=s.f_bsize;
 	if ( tmp < (eventData::CacheSize*12)/10 ) // 20% overhead
 	{
-		eDebug("[EPGC] not enough free space at path '%s' %lld bytes availd but %d needed", buf, tmp, (eventData::CacheSize*12)/10);
+		eDebug("[EPGC] not enough free space at '%s' %lld bytes available but %u needed", buf, tmp, (eventData::CacheSize*12)/10);
 		fclose(f);
 		return;
 	}
