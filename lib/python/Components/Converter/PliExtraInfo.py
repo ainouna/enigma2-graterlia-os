0517ea5fd2546c69fb5fb2ed4a5cd9863daff7b1
Igornord <IgorkaG@gmail.com>
2015-03-25 22:00:30 +0300

    PliExtraInfo: Always fallback to settings info if tuner not tuned (yet)

--- lib/python/Components/Converter/PliExtraInfo.py
+++ lib/python/Components/Converter/PliExtraInfo.py
@@ -142,18 +142,13 @@ class PliExtraInfo(Poll, Converter, object):
 		if onid < 0 : onid = 0
 		return "%d-%d:%05d:%04d:%04d:%04d" % (onid, tsid, sidpid, vpid, apid, pcrpid)
 
-	def createTransponderInfo(self, fedata, feraw, info):
-		if not feraw:
-			feraw = info.getInfoObject(iServiceInformation.sTransponderData)
-			fedata = ConvertToHumanReadable(feraw)
-		if feraw:
-			if "DVB-T" in feraw.get("tuner_type"):
-				tmp = addspace(self.createChannelNumber(fedata, feraw)) + addspace(self.createFrequency(feraw)) + addspace(self.createPolarization(fedata))
-			else:
-				tmp = addspace(self.createFrequency(feraw)) + addspace(self.createPolarization(fedata))
-			return addspace(self.createTunerSystem(fedata)) + tmp + addspace(self.createSymbolRate(fedata, feraw)) + addspace(self.createFEC(fedata, feraw)) \
-				+ addspace(self.createModulation(fedata)) + addspace(self.createOrbPos(feraw))
-		return ""
+	def createTransponderInfo(self, fedata, feraw):
+		if "DVB-T" in feraw.get("tuner_type"):
+			tmp = addspace(self.createChannelNumber(fedata, feraw)) + addspace(self.createFrequency(feraw)) + addspace(self.createPolarization(fedata))
+		else:
+			tmp = addspace(self.createFrequency(feraw)) + addspace(self.createPolarization(fedata))
+		return addspace(self.createTunerSystem(fedata)) + tmp + addspace(self.createSymbolRate(fedata, feraw)) + addspace(self.createFEC(fedata, feraw)) \
+			+ addspace(self.createModulation(fedata)) + addspace(self.createOrbPos(feraw))
 
 	def createFrequency(self, feraw):
 		frequency = feraw.get("frequency")
